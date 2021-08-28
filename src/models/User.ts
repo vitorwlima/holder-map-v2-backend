2eca94814e8fdaaf930be0c6252c381d3c480332
vitorwlima <vitorwlima13@gmail.com>
2021-08-28 20:55:14 -0300

    add campos de empresas no usuario

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,6 +4,8 @@ interface IUser {
   name: string
   email: string
   password: string
+  favoriteCompanies: string[]
+  recentCompanies: string[]
 }
 
 const schema = new Schema<IUser>(
@@ -11,6 +13,8 @@ const schema = new Schema<IUser>(
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
+    favoriteCompanies: [{ type: String }],
+    recentCompanies: [{ type: String }],
   },
   { timestamps: true }
 )
